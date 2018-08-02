4fe25ee24b3086c54ac1a941df1d0d835783ade0
Irfan Maknojia <imak_7@yahoo.com>
2018-08-02 02:08:05 -0500

    Added segues between VC

---
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -0,0 +1,26 @@
+//
+//  WelcomeVC.swift
+//  app-swoosh
+//
+//  Created by Irfan Maknojia on 7/30/18.
+//  Copyright © 2018 Irfan Maknojia. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
+
+}
+
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Irfan Maknojia on 8/2/18.
+//  Copyright © 2018 Irfan Maknojia. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
